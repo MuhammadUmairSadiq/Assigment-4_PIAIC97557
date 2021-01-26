da3fc9966bc59bff24de717e2781e318979cad8c
Umair Sadiq <mohidbuttm22@gmaail.com>
2021-01-26 17:12:58 +0500

    update almot comp.

---
+++ task4.js
@@ -0,0 +1,17 @@
+var email = prompt("Enter your E-mail  Address");
+var n = false;
+if (id.indexof("@")!== -1) {
+    for (let m = 0; m < id.length; m++) {
+        if (id.charAt(m) === (id.charAt(65-90 || id.charAt(97-122))) {
+            if (id.slice(indexof("@")+1,(id.length()-indexOf("@"))) === (id.charAt )) {
+                n = true;
+                break;
+            }   
+        }   
+    }
+    document.write("Valid E-mail ID.");
+}
+else {
+    n = false;
+    document.write("Invalid E-mail ID.");
+}
